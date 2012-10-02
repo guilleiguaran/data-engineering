41991d65a58c8f7a1d6e022bb62f7ef48005a7f8
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-10-02 18:55:07 -0500

    Basic unit test for Import model

--- test/unit/import_test.rb
+++ test/unit/import_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ImportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#create import attached file" do
+    file_path = Rails.root.join("test", "fixtures", "example_input.tab")
+    uploaded_file = Rack::Test::UploadedFile.new(file_path, "text/plain")
+    import = Import.create!(file: uploaded_file)
+
+    assert import.completed?
+    assert_equal 95.0, import.total_amount
+  end
 end
