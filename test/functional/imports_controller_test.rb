625428f8c3928df1f89c1df23946df13e6a56b25
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-10-02 15:45:46 -0500

    Imports: Remove unused views and tests for unexisting actions

--- test/functional/imports_controller_test.rb
+++ test/functional/imports_controller_test.rb
@@ -5,7 +5,7 @@ class ImportsControllerTest < ActionController::TestCase
     @import = imports(:one)
   end
 
-  test "should get index" do
+  test "should list imports list" do
     get :index
     assert_response :success
     assert_not_nil assigns(:imports)
@@ -22,28 +22,5 @@ class ImportsControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to import_path(assigns(:import))
-  end
-
-  test "should show import" do
-    get :show, id: @import
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @import
-    assert_response :success
-  end
-
-  test "should update import" do
-    put :update, id: @import, import: { total_amount: @import.total_amount }
-    assert_redirected_to import_path(assigns(:import))
-  end
-
-  test "should destroy import" do
-    assert_difference('Import.count', -1) do
-      delete :destroy, id: @import
-    end
-
-    assert_redirected_to imports_path
   end
 end
